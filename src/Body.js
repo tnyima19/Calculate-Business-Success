1b8d807ee0c3c52c095f2bdb8ab8b0e6ffac4c09
tnyima19 <66368294+tnyima19@users.noreply.github.com>
2023-05-01 03:09:11 -0400

    "first commit"

---
+++ src/Body.js
@@ -0,0 +1,10 @@
+import React from "react";
+import InitialFindings from "./InitialFindings";
+function Body(){
+    return(<div>
+        <h2>Initial Findings</h2>
+        <InitialFindings/>
+    </div>);
+}
+
+export default Body;
